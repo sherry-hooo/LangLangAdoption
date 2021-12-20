6aaaecfe2dc2f0ba3e9e2643c35c5f3fd1741efc
Rex <smilerex@gmail.com>
2021-12-20 20:28:48 +0800

    first commit

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,5 +1,19 @@
 <template>
-  <main></main>
+  <nav class="navbar_container">
+      <div>
+        <a href="##">
+          <img src="./images/Union.svg" alt="Union" title="浪我陪你等家" />
+        </a>
+      </div>
+      <div>
+        <ul>
+          <li>尋找浪浪</li>
+          <li>追蹤浪浪</li>
+          <li>志工招募</li>
+          <li>愛心項圈</li>
+        </ul>
+      </div>
+    </nav>
 </template>
 
 <style lang="scss" scoped></style>
