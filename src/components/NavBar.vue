d5bdfe7d822eba481ae994aa43980aa95fb00039
Hoo <sherryho148@gmail.com>
2021-12-21 14:59:48 +0800

    add router-link

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -2,17 +2,17 @@
   <header>
     <div class="header_container">
       <div class="navlog">
-        <a href="##">
-          <img src="./images/Union.svg" alt="Union" title="浪我陪你等家" />
-        </a>
+        <router-link to="/">
+          <img src="@/assets/img/Union.svg" alt="Union" title="浪我陪你等家" />
+        </router-link>
       </div>
       <label for="burger">&#9776;</label>
       <input type="checkbox" id="burger" />
       <ul class="navbar">
-        <li>尋找浪浪</li>
-        <li>追蹤浪浪</li>
-        <li>志工招募</li>
-        <li>愛心項圈</li>
+        <li><router-link to="Search">尋找浪浪</router-link></li>
+        <li><router-link to="Track">追蹤浪浪 </router-link></li>
+        <li><router-link to="Volunteer">志工招募 </router-link></li>
+        <li><router-link to="Custom">愛心項圈 </router-link></li>
       </ul>
     </div>
   </header>
