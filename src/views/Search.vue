46ef1bd2080dee4fb81a056cafebc159114a410d
AT <zatchbell.1206@gmail.com>
2021-12-21 20:13:19 +0800

    feat(Search):import components Pagination

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -1,12 +1,15 @@
 <template>
   <Card />
+  <Pagination />
 </template>
 
 <script>
 import Card from "@/components/Card.vue";
+import Pagination from "@/components/Pagination.vue";
 export default {
   components: {
     Card,
+    Pagination,
   },
   data() {
     return {};
