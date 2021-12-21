bfaad229f286db70565c0a757149f57d9313c207
Hoo <sherryho148@gmail.com>
2021-12-21 17:24:53 +0800

    import volunteerApply component

--- src/views/Volunteer.vue
+++ src/views/Volunteer.vue
@@ -1,5 +1,29 @@
 <template>
-  <main></main>
+  <main>
+    <button class="apply" @click="volunteerApply = true">報名加入</button>
+  </main>
+  <VolunteerApply v-if="volunteerApply"></VolunteerApply>
 </template>
 
-<style lang="scss" scoped></style>
+<script>
+import VolunteerApply from "@/components/VolunteerApply.vue";
+export default {
+  components: { VolunteerApply },
+  data() {
+    return {
+      volunteerApply: false,
+    };
+  },
+};
+</script>
+<style lang="scss" scoped>
+.apply {
+  background: color.$brown_300;
+  color: white;
+  font-size: 25px;
+  font-weight: 900;
+  border-radius: 10px;
+  padding: 20px 50px;
+  cursor: pointer;
+}
+</style>
