ea8168b3028d1ea6588e005f1608d0bf02c3e79b
Hoo <sherryho148@gmail.com>
2021-12-22 23:05:54 +0800

    feat(Card): add ending tag

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -4,7 +4,7 @@
       name: 'Description',
       params: { petID: petData.animal_id, petKind: petData.animal_kind },
     }"
-
+  >
     <div class="case_card">
       <div class="case_image">
         <img :src="petData.album_file" alt="浪浪圖片" />
@@ -25,7 +25,6 @@ export default {
   data() {
     return {};
   },
-
 };
 </script>
 
@@ -40,15 +39,13 @@ export default {
     height: 170px;
     box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
 
-
-    @include breakpoint.tablet{
+    @include breakpoint.tablet {
       flex-direction: column;
       width: 270px;
       height: 340px;
     }
 
-
-    @include breakpoint.desktop{
+    @include breakpoint.desktop {
       width: 300px;
       height: auto;
     }
@@ -57,8 +54,6 @@ export default {
       width: 400px;
       height: 425px;
     }
-
-
   }
   &_image {
     flex-basis: 60%;
