129c692e77f466f9d75b2373d4647a24329b1697
Rex <smilerex@gmail.com>
2021-12-25 01:49:28 +0800

    feat(Description view): modify RWD setting

--- src/views/Description.vue
+++ src/views/Description.vue
@@ -2,17 +2,20 @@
   <main>
     <div class="description">
       <div class="des_container">
-        <div>
-          <h2>浪浪資料</h2>
-          <div class="circle_box">
-            <img src="@/assets/img/circles.svg" alt="" />
+        <h2>浪浪資料</h2>
+        <div class="line_box">
+          <div class="line2_box">
+            <img src="@/assets/img/Line2.svg" alt="" />
+          </div>
+          <div class="line3_box">
+            <img src="@/assets/img/Line3.svg" alt="" />
           </div>
         </div>
         <div class="case">
+          <div class="circle_box">
+            <img src="@/assets/img/circles.svg" alt="" />
+          </div>
           <div class="case_img_box">
-            <!-- <div class="circle_box">
-              <img src="@/assets/img/circles.svg" alt="" />
-            </div> -->
             <div class="img_box">
               <img :src="petData.album_file" alt="浪浪圖片" />
             </div>
@@ -52,6 +55,13 @@
               <p class="case_title">體型 &colon;</p>
               <p class="case_content">{{ petData.animal_bodytype }}</p>
             </div>
+            <div
+              v-if="petData.animal_createtime"
+              class="case_wrapper wrapper_2"
+            >
+              <p class="case_title">入所日期 &colon;</p>
+              <p class="case_content">{{ petData.animal_createtime }}</p>
+            </div>
             <div v-if="petData.animal_place" class="case_wrapper wrapper_1">
               <p class="case_title">收容地點 &colon;</p>
               <p class="case_content">{{ petData.animal_place }}</p>
@@ -63,13 +73,6 @@
             <div v-if="petData.shelter_tel" class="case_wrapper wrapper_2">
               <p class="case_title">聯絡電話 &colon;</p>
               <p class="case_content">{{ petData.shelter_tel }}</p>
-            </div>
-            <div
-              v-if="petData.animal_createtime"
-              class="case_wrapper wrapper_1"
-            >
-              <p class="case_title">入所日期 &colon;</p>
-              <p class="case_content">{{ petData.animal_createtime }}</p>
             </div>
             <div v-if="petData.animal_remark" class="case_wrapper">
               <p class="case_title">備註 &colon;</p>
@@ -196,12 +199,15 @@ export default {
   cursor: pointer;
 }
 main {
-  padding: 60px 0px 0px 0px;
+  padding-top: 60px;
   background-color: color.$brown-100;
   position: relative;
   overflow: hidden;
   height: fit-content;
   min-height: 100vh;
+  @include breakpoint.tablet {
+    padding-top: 103px;
+  }
 }
 .description {
   padding: 10px 30px 30px 30px;
@@ -225,6 +231,7 @@ main {
   }
 }
 .des_container {
+  position: relative;
   display: flex;
   flex-direction: column;
   margin: 0 auto;
@@ -233,14 +240,16 @@ main {
   }
 }
 .case {
-  margin-top: 20px;
+  margin-top: 10px;
+  @include breakpoint.tablet {
+    margin-top: 30px;
+  }
   @include breakpoint.desktop {
-    margin-top: 60px;
+    margin-top: 30px;
     display: flex;
     justify-content: space-between;
-    gap: 30px;
+    gap: 80px;
   }
-
   &_img_box {
     display: flex;
     flex-direction: column;
@@ -252,8 +261,7 @@ main {
     @include breakpoint.tablet {
     }
     @include breakpoint.desktop {
-      flex-direction: column;
-      flex: 50%;
+      display: block;
     }
   }
   &_wrapper {
@@ -269,48 +277,116 @@ main {
   }
   & .wrapper_1 {
     flex: 60%;
+    @include breakpoint.desktop {
+      flex: 40%;
+    }
   }
   &_des_box {
     display: flex;
     flex-wrap: wrap;
     margin-top: 30px;
     text-align: left;
-    gap: 10px;
+    gap: 10px 50px;
+    @include breakpoint.mobile {
+      // max-width: 576px;
+      margin: 0 auto;
+      margin-top: 40px;
+    }
+    @include breakpoint.tablet {
+      gap: 15px;
+    }
     @include breakpoint.desktop {
+      margin-top: 0;
+      flex-direction: column;
+      flex-wrap: nowrap;
       flex: 50%;
     }
   }
   &_title {
-    font-size: 24px;
-    font-weight: 900;
+    font-size: 22px;
+    font-weight: 500;
     color: color.$gray_700;
+    @include breakpoint.desktop {
+      font-size: 25px;
+    }
   }
   &_content {
+    margin-top: 5px;
     font-size: 18px;
-    font-weight: 900;
+    font-weight: 400;
     color: color.$gray_500;
+    @include breakpoint.desktop {
+      font-size: 21px;
+    }
   }
 }
 .circle_box {
-  position: absolute;
-  right: -25px;
-  top: 478px;
-  width: 30%;
+  display: none;
+  @include breakpoint.mobile {
+    display: block;
+    position: absolute;
+    width: 10%;
+    left: 35vw;
+    top: 31vh;
+  }
+  @include breakpoint.tablet {
+    left: 35vw;
+    top: 44vh;
+  }
+  @include breakpoint.desktop {
+    left: 30vw;
+    top: 48vh;
+  }
+  @include breakpoint.bgScreen {
+    left: 30vw;
+    top: 48vh;
+  }
   & img {
     width: 100%;
+    height: 100%;
+  }
+}
+.line_box {
+  display: none;
+  @include breakpoint.mobile {
+    display: block;
+  }
+}
+
+.line2_box {
+  position: absolute;
+  right: 0;
+  top: 3px;
+  width: 170px;
+  & img {
+    width: 100%;
+    height: 100%;
+  }
+}
+.line3_box {
+  position: absolute;
+  right: 15px;
+  top: 0;
+  height: 170px;
+  & img {
+    height: 100%;
   }
 }
 .img_box {
-  flex: 50%;
+  flex: 40%;
   border: solid 8px;
   border-color: color.$white;
   background-color: color.$brown_100;
   box-shadow: 3px 3px 3px rgba(0, 0, 0, 0.4);
   overflow: hidden;
   max-width: 500px;
-  max-height: 400px;
+  // max-height: 400px;
   & img {
     width: 100%;
+    height: 100%;
+  }
+  @include breakpoint.desktop {
+    flex: unset;
   }
 }
 .follow_box {
@@ -324,11 +400,9 @@ main {
     gap: 30px;
     justify-content: flex-end;
   }
-  @include breakpoint.tablet {
-    flex: unset;
-  }
   @include breakpoint.desktop {
-    flex: 40%;
+    margin-top: 50px;
+    justify-content: flex-start;
   }
 }
 .follow {
@@ -364,31 +438,43 @@ main {
   font-weight: 500;
   color: color.$white;
   cursor: pointer;
-  @include breakpoint.tablet {
-    font-size: 25px;
+  @include breakpoint.mobile {
+    margin-left: unset;
+  }
+  // @include breakpoint.tablet {
+  // }
+  @include breakpoint.bgScreen {
+    font-size: 22px;
+    padding: 15px 30px;
   }
 }
 .look_more {
   margin-top: 30px;
+  text-align: end;
   a {
     display: block;
     &::before {
       content: "";
-      height: 2px;
+      height: 1px;
       display: inline-block;
       background-color: color.$brown_300;
-      margin: 13px auto;
+      @include breakpoint.mobile {
+        width: 200px;
+      }
       @include breakpoint.tablet {
-        width: 300px;
+        width: 250px;
+      }
+      @include breakpoint.desktop {
+        width: 350px;
       }
     }
   }
   & span {
-    font-size: 30px;
-    font-weight: 900;
+    font-size: 24px;
+    font-weight: 500;
     color: color.$brown_500;
     @include breakpoint.tablet {
-      font-size: 40px;
+      font-size: 30px;
     }
   }
   .arrow_box {
@@ -396,10 +482,18 @@ main {
     max-width: 75px;
     & img {
       width: 100%;
+      height: 100%;
     }
     @include breakpoint.tablet {
-      max-width: 175px;
+      max-width: 120px;
     }
+  }
+}
+.line4_box {
+  display: inline-block;
+  width: 36%;
+  & img {
+    width: 100%;
   }
 }
 </style>
