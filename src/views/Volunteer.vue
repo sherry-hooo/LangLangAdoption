1b5589a0fb9e56599650256841a242c560f20fcb
Rex <smilerex@gmail.com>
2021-12-26 13:26:12 +0800

    feat(Volunteer view): animation

--- src/views/Volunteer.vue
+++ src/views/Volunteer.vue
@@ -4,9 +4,19 @@
       <h2>志工招募</h2>
       <div class="flex_1">
         <div class="layout_line_1">
-          <div class="solid_circle"></div>
+          <div class="wave_box">
+            <div class="wave">
+              <div class="circle"></div>
+              <div class="solid_circle"></div>
+            </div>
+          </div>
           <div class="frame_circle"></div>
         </div>
+
+        <!-- <div class="layout_line_1">
+          <div class="solid_circle"></div>
+          <div class="frame_circle"></div>
+        </div> -->
         <div class="img_box">
           <img src="@/assets/img/volunteer1.jpg" alt="" />
         </div>
@@ -288,34 +298,34 @@ h2 {
     }
   }
 }
-.layout_line_1 {
-  position: absolute;
-  top: 32%;
-  left: 20%;
-  @include breakpoint.mobile {
-    top: 59%;
-  }
-  @include breakpoint.tablet {
-    top: 42%;
-  }
-  @include breakpoint.desktop {
-    top: 50%;
-  }
-}
-.solid_circle {
-  border-radius: 999em;
-  background-color: #af9380;
-  width: 25px;
-  height: 25px;
-  transform: translate(60px, 90px);
-}
-.frame_circle {
-  border: solid 2px;
-  border-radius: 999em;
-  border-color: #dec39e;
-  width: 70px;
-  height: 70px;
-}
+// .layout_line_1 {
+//   position: absolute;
+//   top: 32%;
+//   left: 20%;
+//   @include breakpoint.mobile {
+//     top: 59%;
+//   }
+//   @include breakpoint.tablet {
+//     top: 42%;
+//   }
+//   @include breakpoint.desktop {
+//     top: 50%;
+//   }
+// }
+// .solid_circle {
+//   border-radius: 999em;
+//   background-color: #af9380;
+//   width: 25px;
+//   height: 25px;
+//   transform: translate(60px, 90px);
+// }
+// .frame_circle {
+//   border: solid 2px;
+//   border-radius: 999em;
+//   border-color: #dec39e;
+//   width: 70px;
+//   height: 70px;
+// }
 .layout_line_2 {
   position: absolute;
   top: -50px;
@@ -359,6 +369,98 @@ h2 {
     border-radius: 10px;
     padding: 15px 30px;
     cursor: pointer;
+  }
+}
+/* * {
+  outline: solid 1px;
+} */
+.layout_line_1 {
+  position: absolute;
+  top: 32%;
+  left: 20%;
+  @include breakpoint.mobile {
+    top: 59%;
+  }
+  @include breakpoint.tablet {
+    top: 42%;
+  }
+  @include breakpoint.desktop {
+    top: 48%;
+  }
+  @include breakpoint.bgScreen {
+    top: 55%;
+  }
+}
+.wave_box {
+  position: absolute;
+  top: 60px;
+  left: 70px;
+  width: 40%;
+  height: 40%;
+}
+.solid_circle {
+  position: absolute;
+  z-index: 999;
+  border-radius: 999em;
+  background-color: #af9380;
+  width: 25px;
+  height: 25px;
+  /*   filter: drop-shadow(10px 10px 4px rgba(0, 0, 0, 0.1)); */
+  animation: living 3s infinite ease-in-out;
+  border-radius: 50%;
+  padding: 0;
+
+  /*  color:#fff; */
+}
+.frame_circle {
+  border: solid 2px;
+  border-radius: 999em;
+  border-color: #dec39e;
+  width: 70px;
+  height: 70px;
+  /*   filter: drop-shadow(10px 10px 4px rgba(0, 0, 0, 0.3)); */
+  animation: living 3s infinite ease-in-out;
+  /*   animation: 2s infinite alternate living; */
+}
+
+/* 波動效果 */
+.wave {
+  position: relative;
+  width: 25px;
+  height: 25px;
+  text-align: center;
+
+  /*   filter: drop-shadow(0 0 0.75rem crimson); */
+}
+/* .wave .circle {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  border-radius: 50%;
+  opacity: 0;
+  background: #af9380;
+} 
+.wave .circle:first-child { 
+   animation: circle-opacity 2s infinite;
+} */
+@keyframes circle-opacity {
+  from {
+    opacity: 1;
+    transform: scale(1);
+  }
+  to {
+    opacity: 0;
+    transform: scale(2);
+  }
+}
+@keyframes living {
+  from {
+    opacity: 1;
+    transform: scale(1);
+  }
+  to {
+    opacity: 0;
+    transform: scale(2);
   }
 }
 </style>
