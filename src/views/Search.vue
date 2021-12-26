cafab1431970840deddc80cce5877cec42e43af3
Hoo <sherryho148@gmail.com>
2021-12-26 09:48:15 +0800

    fix dropdown select issue

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -3,10 +3,10 @@
     <h3>尋找浪浪</h3>
     <Dropdown
       @selectedAnimalKind="receiveAnimalKind"
-      @selectedShelter="receiveSelectedShelter"
       @selection="receiveSelection"
-      :filteredShelter="filteredShelter"
+      :shelterList="shelterList"
     ></Dropdown>
+    <!-- @selecztedShelter="receiveSelectedShelter" -->
     <section>
       <Card v-for="petData in petData" :key="petData" :petData="petData"></Card>
     </section>
@@ -32,37 +32,42 @@ export default {
   },
   data() {
     return {
-      petsDataInArray: [],
+      singleKindData: [],
+      singleShelterData: [],
       selectedAnimanlKind: "",
       currentPage: 1,
       cardAmount: 9,
-      selectedShelter: "",
+      selectedShelterPkid: "",
+      shelterList: [],
     };
   },
   methods: {
     getAnimalAPI(kind) {
-      getApi
+      return getApi
         .getAnimalData(kind)
-        .then((res) => (this.petsDataInArray = res.data.Data));
+        .then((res) => (this.singleKindData = res.data.Data));
     },
     getShelterAPI(kind, shelterPkid) {
       getApi
         .getShelterData(kind, shelterPkid)
-        .then((res) => (this.petsDataInArray = res.data.Data));
+        .then((res) => (this.singleShelterData = res.data.Data));
     },
-    receiveAnimalKind(data) {
+    // 接收選取的動物類別
+    async receiveAnimalKind(data) {
+      await this.getAnimalAPI(data);
       this.selectedAnimanlKind = data;
-      this.getAnimalAPI(data);
+      console.log("test");
+      this.filteredShelterByKind(this.petsDataInArray);
     },
-    receiveSelectedShelter(shelterPkid) {
-      this.selectedShelter = shelterPkid;
-
-      this.getShelterAPI(this.selectedAnimanlKind, shelterPkid);
-    },
+    // 接收選的 filter
+    // receiveSelectedShelter(shelterPkid) {
+    //   this.selectedShelter = shelterPkid;
+    //   this.getShelterAPI(this.selectedAnimanlKind, shelterPkid);
+    // },
     receiveSelection(data) {
       this.selectedAnimanlKind = data.animalKind;
-      this.selectedShelter = data.shelterPlace;
-      this.getShelterAPI(data.animalKind, data.shelterPlace);
+      this.selectedShelterPkid = data.shelterPkid;
+      this.getShelterAPI(data.animalKind, data.shelterPkid);
     },
     receiveCurrentPage(page) {
       this.currentPage = page;
@@ -74,22 +79,10 @@ export default {
         behavior: "smooth",
       });
     },
-  },
-  computed: {
-    petData() {
-      return this.petsDataInArray.slice(this.startIndex, this.endIndex);
-    },
-    startIndex() {
-      return (this.currentPage - 1) * this.cardAmount;
-    },
-    endIndex() {
-      return this.currentPage * this.cardAmount;
-    },
-    totalPage() {
-      return parseInt(this.petsDataInArray.length / this.cardAmount);
-    },
-    filteredShelter() {
-      let getShelterData = this.petsDataInArray.map((data) => ({
+    // 依照動物類別篩選出收容所
+    filteredShelterByKind() {
+      console.log(this.singleKindData);
+      let getShelterData = this.singleKindData.map((data) => ({
         shelter_name: data.shelter_name,
         animal_shelter_pkid: data.animal_shelter_pkid,
       }));
@@ -97,7 +90,7 @@ export default {
       let sortedShelter = getShelterData.sort((front, back) => {
         return front.animal_shelter_pkid - back.animal_shelter_pkid;
       });
-      return sortedShelter.reduce((accumulator, current) => {
+      this.shelterList = sortedShelter.reduce((accumulator, current) => {
         if (
           accumulator.length === 0 ||
           accumulator[accumulator.length - 1].animal_shelter_pkid !==
@@ -108,6 +101,41 @@ export default {
         return accumulator;
       }, []);
     },
+  },
+  computed: {
+    petData() {
+      return this.singleShelterData.slice(this.startIndex, this.endIndex);
+    },
+    startIndex() {
+      return (this.currentPage - 1) * this.cardAmount;
+    },
+    endIndex() {
+      return this.currentPage * this.cardAmount;
+    },
+    totalPage() {
+      return parseInt(this.singleShelterData.length / this.cardAmount);
+    },
+    // shelterList() {
+    //   let getShelterData = this.singleKindData.map((data) => ({
+    //     shelter_name: data.shelter_name,
+    //     animal_shelter_pkid: data.animal_shelter_pkid,
+    //   }));
+
+    //   let sortedShelter = getShelterData.sort((front, back) => {
+    //     return front.animal_shelter_pkid - back.animal_shelter_pkid;
+    //   });
+
+    //   return sortedShelter.reduce((accumulator, current) => {
+    //     if (
+    //       accumulator.length === 0 ||
+    //       accumulator[accumulator.length - 1].animal_shelter_pkid !==
+    //         current.animal_shelter_pkid
+    //     ) {
+    //       accumulator.push(current);
+    //     }
+    //     return accumulator;
+    //   }, []);
+    // },
   },
 };
 </script>
